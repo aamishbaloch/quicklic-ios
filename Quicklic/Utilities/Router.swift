5e2d4cc820482865ff5ff065f0d40aeb20a137d5
Danial Zahid <danialzahid94@live.com>
2017-08-22 12:34:08 +0500

    Initial Commit

---
+++ Quicklic/Utilities/Router.swift
@@ -0,0 +1,15 @@
+//
+//  Router.swift
+//  Map Hunt
+//
+//  Created by Danial Zahid on 16/08/2017.
+//  Copyright © 2017 Fitsmind. All rights reserved.
+//
+
+import UIKit
+
+class Router: NSObject {
+    
+    
+    
+}
