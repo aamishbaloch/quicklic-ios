b53c7ed60d1740682429ef0dfd37dd6f933d9230
Danial Zahid <danialzahid94@live.com>
2018-01-10 11:29:38 +0500

    New bug fixes

--- Quicklic/Utilities/Constants.swift
+++ Quicklic/Utilities/Constants.swift
@@ -17,8 +17,8 @@ public class Constant: NSObject {
     static let appAppointmentDateFormat = "MMM dd',' yyyy"
     static let animationDuration : TimeInterval = 0.5
     
-//    static let serverURL = "http://staging.quicklic.com/api/v1/"
-    static let serverURL = "http://5fef7a8c.ngrok.io/api/v1/"
+    static let serverURL = "http://staging.quicklic.com/api/v1/"
+//    static let serverURL = "http://5fef7a8c.ngrok.io/api/v1/"
     
     static let mainColor = UIColor(red: 145.0/255.0, green: 20.0/255.0, blue: 217.0/255.0, alpha: 1.0)
     
